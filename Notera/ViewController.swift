76b2de50a4a62ec0fe95c4a75dc38b598b8f6dd3
Wilwous <mick.thomson.tm@gmail.com>
2024-08-01 14:04:01 +0300

    Initial Commit

---
+++ Notera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notera
+//
+//  Created by Антон Павлов on 01.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
